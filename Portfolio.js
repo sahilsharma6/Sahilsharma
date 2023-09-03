786f7651bf7217f012dfabfa630c0981c18330de
sahilsharma6 <sahilsharma86600@gmail.com>
2023-09-03 20:31:41 +0530

    created top to back

--- Portfolio.js
+++ Portfolio.js
@@ -11,6 +11,41 @@ function hamMenu(){
     
   }
 
+
+
+
+  let mybutton = document.getElementById("myBtn");
+
+  // When the user scrolls down 20px from the top of the document, show the button
+  window.onscroll = function() {scrollFunction()};
+  
+  function scrollFunction() {
+    if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+      mybutton.classList.add('active')
+    } else {
+      // mybutton.style.display = "none";
+      mybutton.classList.remove('active')
+
+    }
+  }
+  
+  // When the user clicks on the button, scroll to the top of the document
+  function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+  }
+
+
+
+
+
+
+
+
+let input = document.getElementsByTagName("input"); 
+
+console.log(input[0]);
+
   
 
   const scriptURL = 'https://script.google.com/macros/s/AKfycbySpdd8fBCJSa_JR0yu1cn6FdlJKBj01N5zN_0V7IEbsbVfv98Y62qBdVpKyIm1wjjpTg/exec';
@@ -20,6 +55,10 @@ function hamMenu(){
             form.addEventListener('submit', e => {
               e.preventDefault()
               fetch(scriptURL, { method: 'POST', body: new FormData(form)})
-                .then(response => alert("Thanks for Contacting us..! We Will Contact You Soon..."))
+                .then(response => alert("Thanks for Contacting us..! We Will Contact You Soon...")
+                
+                // input[0].value = ""
+                
+                )
                 .catch(error => console.error('Error!', error.message))
             })
